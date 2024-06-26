d27e8ddcd46d8e3f308ae0164298452feeff2cc0
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-06-26 01:07:56 -0500

    finish commit

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,27 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        const walkerId = clickTarget.dataset.wfk
+        const walkers = getWalkers()
+        for (const walker of walkers) {
+            if(walker.id === parseInt(walkerId)){
+                window.alert(`This pet is being walked by ${walker.name}`)
+            }
+            
+        }
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-wfk="${pet.walkerId}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -2,12 +2,26 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const theClick = clickEvent.target
+
+        if (theClick.dataset.type === "walker") {
+            window.alert(`This walker works in ${theClick.dataset.city}`)
+        }
+    }
+)
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li data-id="${walker.id}"
+                         data-city="${walker.city}"
+                         data-type="walker"
+                        >${walker.name}
+        </li>`
     }
 
     walkerHTML += "</ul>"
